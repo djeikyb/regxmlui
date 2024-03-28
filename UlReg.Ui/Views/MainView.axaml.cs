7585917c1c74c17a78bdbb20deca3872e6a01165
Jacob Certain <jace42@gmail.com>
2024-03-27 20:56:40 -0700

    example of binding in the codebehind

--- UlReg.Ui/Views/MainView.axaml.cs
+++ UlReg.Ui/Views/MainView.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data;
+using UlReg.ViewModels;
 
 namespace UlReg.Views;
 
@@ -7,5 +10,13 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+        // this.DataContextChanged += (sender, args) =>
+        // {
+        //     var view = (MainView)sender!;
+        //     var main = (MainViewModel)view.DataContext!;
+        //     var vm = main.TryMeVm;
+        //     var binding = new Binding { Source = vm.SearchTerm, Path = nameof(vm.SearchTerm.Value) };
+        //     InputSearchTerm.Bind(TextBlock.TextProperty, binding);
+        // };
     }
 }
