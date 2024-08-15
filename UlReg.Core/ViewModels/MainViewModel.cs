f6577aa5744647fe3cf9068e65af9f18c9da25c9
Jacob Certain <jace42@gmail.com>
2024-08-15 00:59:16 -0700

    move open pdf logic to view model

--- UlReg.Core/ViewModels/MainViewModel.cs
+++ UlReg.Core/ViewModels/MainViewModel.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 using ObservableCollections;
 using R3;
 using RegXml;
@@ -7,7 +9,7 @@ using UlRegBiz.Services;
 
 namespace UlReg.ViewModels;
 
-public class MainViewModel : IDisposable
+public partial class MainViewModel : IDisposable
 {
     private readonly IRegisterService _register;
     private readonly ObservableList<RegisterEntryViewModel> _entries;
@@ -51,6 +53,65 @@ public class MainViewModel : IDisposable
             var item = v[SelectedRowIndex.Value];
             if (item.Ul is not null) appService.SetClipboardText(item.Ul);
         });
+        OpenDefiningDocument = new();
+        OpenDefiningDocument.Subscribe(maybeDoc =>
+        {
+
+                var parsed = ParseDefiningDocumentField(maybeDoc);
+                if (parsed is null) return;
+                var (cat, num) = parsed.Value;
+                Console.WriteLine($"📂 will try to open: {cat} {num}");
+
+                var location = $"/Users/jacob/www/smpte/pub.smpte.org/pub.smpte.org/doc/{cat}{num}";
+                var files = Directory.EnumerateFiles(location, "*.pdf",
+                        new EnumerationOptions
+                        {
+                            RecurseSubdirectories = true,
+                            MatchCasing = MatchCasing.CaseInsensitive,
+                            MatchType = MatchType.Simple,
+                        })
+                    .Where(pdf =>
+                    {
+                        // if the regxml defining document says "amendment"
+                        // then go ahead and include amendment pdfs
+                        // but otherwise
+                        // they're just cluttering up the result space
+                        // so exclude 'em
+                        if (pdf.Contains("-am") && !maybeDoc.Contains("amendment")) return false;
+                        return true;
+                    })
+                    .GroupBy(
+                        k =>
+                        {
+                            var fn = Path.GetFileName(k);
+                            fn = Regex.Replace(fn, @"^st0", "st");
+                            fn = Regex.Replace(fn, @"-20\d\d.pdf", "-20xx.pdf"); // TOneverDO century bug
+                            return fn;
+                        },
+                        v => v)
+                    .Select(g => g.Last())
+                    .Select(x => $"file://{x}")
+                    .ToList();
+
+                if (files.Count == 1)
+                {
+                    using var _ = Process.Start(new ProcessStartInfo("open", files[0]) { UseShellExecute = true });
+                }
+                else
+                {
+                    using var _ = Process.Start(new ProcessStartInfo(location) { UseShellExecute = true });
+                }
+
+                Console.WriteLine($"\tfound:\n{string.Join('\n', files.Select(x => $"\t\t{x}"))}");
+
+                // if (maybeDoc != null && maybeDoc.StartsWith("SMPTE"))
+                // {
+                //     var r = new Regex(".*SMPTE ?(?:number.*) ?.*$",
+                //         RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+                //     var location = "/Users/jacob/www/smpte/pub.smpte.org/pub.smpte.org/doc";
+                //     Directory.EnumerateFiles(location, "**.pdf", SearchOption.AllDirectories);
+                // }
+        });
     }
 
     public BindableReactiveProperty<string?> SearchTerm { get; }
@@ -62,6 +123,7 @@ public class MainViewModel : IDisposable
     public INotifyCollectionChangedSynchronizedView<RegisterEntryViewModel> EntriesView { get; }
     public BindableReactiveProperty<int> SelectedRowIndex { get; }
     public ReactiveCommand<Unit> CopyCommand { get; }
+    public ReactiveCommand<string> OpenDefiningDocument { get; }
 
     internal void RefreshTable()
     {
@@ -85,6 +147,113 @@ public class MainViewModel : IDisposable
                     .Select(re => new RegisterEntryViewModel(re)));
         }
     }
+
+    private (string cat, string num)? ParseDefiningDocumentField(string defdoc)
+    {
+        if ("377M-1".Equals(defdoc)) return ("ST", "377-1");
+        if ("SMPTE 377M".Equals(defdoc)) return ("ST", "377-1");
+        if ("SMPTE 377M-1".Equals(defdoc)) return ("ST", "377-1");
+        var m = SmpteDocNameBasic().Match(defdoc);
+
+        if (m.Groups["cat"].Captures.Count > 1)
+        {
+            Console.WriteLine("⛔️ found more than one cat");
+            return null;
+        }
+
+        if (m.Groups["num"].Captures.Count > 1)
+        {
+            Console.WriteLine("⛔️ found more than one spec number");
+            return null;
+        }
+
+        if (m.Groups["cat"].Captures.Count == 0)
+        {
+            Console.WriteLine("⛔️ missing cat");
+            return null;
+        }
+
+        if (m.Groups["num"].Captures.Count == 0)
+        {
+            Console.WriteLine("⛔️ missing spec number");
+            return null;
+        }
+
+        string cat = m.Groups["cat"].Value switch
+        {
+            "SMPTE" => "ST",
+            { } s => s,
+        };
+
+        string num = m.Groups["num"].Value switch
+        {
+            { } s when s.Contains('.') => s.Replace('.', '-'),
+            { } s => s,
+        };
+
+        return (cat, num);
+    }
+
+    [GeneratedRegex(
+        pattern:
+        @"^.*(?<cat>SMPTE|ST|RDD|RP) ?(?<num>\d+(-|\.)?\d*).*$",
+        options: RegexOptions.IgnoreCase,
+        cultureName: "en-US"
+    )]
+    private static partial Regex SmpteDocNameBasic();
+
+    [GeneratedRegex(
+        pattern:
+        @"(?<whole>SMPTE (?<cat>ST|RP|RDD) (?<num>\d+-?\d*).*)",
+        options: RegexOptions.IgnoreCase,
+        cultureName: "en-US"
+    )]
+    private static partial Regex SmpteDocNames1();
+
+
+    // starts with smpte, maybe has a category
+    // like
+    // - SMPTE ST2067-2
+    // - SMPTE RP2089
+    // - SMPTE 430-6 D-Cinema Auditorium Security Messages
+    // - SMPTE 429-6
+    [GeneratedRegex(
+        pattern:
+        @"(?<whole>SMPTE (?<cat>ST|RP|RDD)?(?<num>\d+[a-zA-Z]*-?\d*).*)",
+        options: RegexOptions.IgnoreCase,
+        cultureName: "en-US"
+    )]
+    private static partial Regex SmpteDocNames2();
+
+
+    // eg | engineering guideline
+    // rdd | registered disclosure document
+    // rp | recommended practice
+    // ov | overview
+    // st | standard
+    //
+    //  59 eg
+    //  17 ov
+    //  52 rdd
+    // 231 rp
+    // 526 st
+    //
+    // so the common forms should be (extra text could be at start or end):
+    //
+    // SMPTE 123
+    // SMPTE 123.4
+    // SMPTE 123-4
+    //
+    // SMPTE (RDD|RP|ST) 123
+    // SMPTE (RDD|RP|ST) 123.4
+    // SMPTE (RDD|RP|ST) 123-4
+    //
+    // so maybe
+    //
+    // ^.*(?<cat>SMPTE|ST|RDD|RP) ?(?<num>\d+.*?)$
+    //
+    // .*SMPTE ST \d+.*? ?
+    // .*SMPTE RDD \d+.*? ?
 
     public void Dispose()
     {
