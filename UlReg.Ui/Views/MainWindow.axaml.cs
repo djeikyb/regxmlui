348fda02042cd6f76cdc3384021702b31c9be070
Jacob Certain <jace42@gmail.com>
2024-08-23 18:07:04 -0700

    use apple system font; add font size key bindings

--- UlReg.Ui/Views/MainWindow.axaml.cs
+++ UlReg.Ui/Views/MainWindow.axaml.cs
@@ -7,6 +7,8 @@ namespace UlReg.Views;
 
 public partial class MainWindow : Window
 {
+    private double _defaultFontSize;
+
     public MainWindow()
     {
         InitializeComponent();
@@ -27,5 +29,24 @@ public partial class MainWindow : Window
             })
         });
 
+        _defaultFontSize = FontSize;
+
+        KeyBindings.Add(new KeyBinding
+        {
+            Gesture = new KeyGesture(Key.OemPlus, KeyModifiers.Meta),
+            Command = new ReactiveCommand<Unit>(_ => FontSize += 0.5),
+        });
+
+        KeyBindings.Add(new KeyBinding
+        {
+            Gesture = new KeyGesture(Key.OemMinus, KeyModifiers.Meta),
+            Command = new ReactiveCommand<Unit>(_ => FontSize -= 0.5),
+        });
+
+        KeyBindings.Add(new KeyBinding
+        {
+            Gesture = new KeyGesture(Key.D0, KeyModifiers.Meta),
+            Command = new ReactiveCommand<Unit>(_ => FontSize = _defaultFontSize),
+        });
     }
 }
