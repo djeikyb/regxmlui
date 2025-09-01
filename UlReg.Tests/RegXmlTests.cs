1abd7090bb7a6f98552ff329c2780015aecdda11
jacob <jace42@gmail.com>
2025-09-01 14:49:53 -0700

    remove xdocument sanity checks

--- UlReg.Tests/RegXmlTests.cs
+++ UlReg.Tests/RegXmlTests.cs
@@ -1,7 +1,5 @@
 using System.IO.Compression;
 using System.Reflection;
-using System.Xml.XPath;
-using FluentAssertions;
 using RegXml;
 using Xunit.Abstractions;
 
@@ -42,61 +40,5 @@ public class RegXmlTests
         {
             _testOutputHelper.WriteLine(entry.FullName);
         }
-    }
-
-    [Fact]
-    public void RegAllTheIsters()
-    {
-        var registers = Registers.FromEmbedded();
-        Assert.NotNull(registers.Groups);
-        var xpath = $"//*[local-name() = 'UL' and text() = 'urn:smpte:ul:060e2b34.027f0101.0d010101.01014e00']/..";
-        var el = registers.Groups._doc.XPathSelectElement(xpath);
-        Assert.NotNull(el);
-        var symbol = el.Descendants().First(x => "Symbol".Equals(x.Name.LocalName)).Value;
-        Assert.Equal("AuxiliaryDescriptor", symbol);
-    }
-
-    [Fact]
-    public void SymbolAlwaysExists()
-    {
-        bool alwaysExists = true;
-        var reg = Registers.FromEmbedded();
-        foreach (var r in reg.All)
-            if (!r.EntryPropertyAlwaysExists("Symbol"))
-                alwaysExists = false;
-        alwaysExists.Should().BeTrue();
-    }
-
-    [Fact]
-    public void RegisterAlwaysExists()
-    {
-        bool alwaysExists = true;
-        var reg = Registers.FromEmbedded();
-        foreach (var r in reg.All)
-            if (!r.EntryPropertyAlwaysExists("Register"))
-                alwaysExists = false;
-        alwaysExists.Should().BeTrue();
-    }
-
-    [Fact]
-    public void UlAlwaysExists()
-    {
-        bool alwaysExists = true;
-        var reg = Registers.FromEmbedded();
-        foreach (var r in reg.All)
-            if (!r.EntryPropertyAlwaysExists("UL"))
-                alwaysExists = false;
-        alwaysExists.Should().BeTrue();
-    }
-
-    [Fact]
-    public void DefinitionDoesNotAlwaysExist()
-    {
-        bool alwaysExists = true;
-        var reg = Registers.FromEmbedded();
-        foreach (var r in reg.All)
-            if (!r.EntryPropertyAlwaysExists("Definition"))
-                alwaysExists = false;
-        alwaysExists.Should().BeFalse();
     }
 }
